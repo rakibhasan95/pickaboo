2495054aff50e4131dd2e6551371db7d8cedef5e
Shakil <shakil.hossain@enkaizen.com>
2023-09-08 23:51:06 +0600

    Initial commit

---
+++ js/myScript.js
@@ -0,0 +1,15 @@
+//Counting section script
+$(document).ready(function () {
+
+	$("#counter-one").animateNumbers(200, true, 5000);
+	$("#counter-two").animateNumbers(50, true, 5000);
+	$("#counter-three").animateNumbers(500, true, 5000);
+	$("#counter-four").animateNumbers(25, true, 5000);
+
+});
+
+// Portfolio section Script 
+$("#portfolio-image").liquo({
+	menu: "#portfolio-menu",
+	random: true
+});
